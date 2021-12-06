5fc7b093e9b98a06ea5f3d6ec76fe8f25e39e56d
JeremyEllingham <jeremy@sethindustries.com.au>
2021-12-06 20:41:08 +0000

    Add Build page.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,6 +33,7 @@ module.exports = {
         { to: "/faqs", label: "FAQs", position: "left" },
         { to: "/stats", label: "Stats", position: "left" },
         { to: "/code", label: "Code", position: "left" },
+        { to: "/build", label: "Build", position: "left" },
         { to: "/recommended", label: "Recommended", position: "left" },
       ],
     },
@@ -65,6 +66,10 @@ module.exports = {
             {
               label: "Code",
               to: "/code",
+            },
+            {
+              label: "Build",
+              to: "/build",
             },
             {
               label: "Recommended",
