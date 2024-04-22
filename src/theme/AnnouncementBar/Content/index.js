d42267f9e27bbef8d7e7ffb99d3b447ff1f6662f
Jeremy <jeremy@Admins-MacBook-Pro.local>
2024-04-22 22:31:34 +0100

    Add Telegram.

--- src/theme/AnnouncementBar/Content/index.js
+++ src/theme/AnnouncementBar/Content/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import youtubeLogo from "@site/static/img/socialMedia/youtube.png";
 import twitterLogo from "@site/static/img/socialMedia/twitter.png";
+import telegramLogo from "@site/static/img/socialMedia/telegram.png";
 
 export default function AnnouncementBarContent() {
   const styles = { height: 60, minHeight: 60, width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'  }
@@ -17,6 +18,11 @@ return <div style={styles}>
       <span style={spanStyle}>
       <a href="https://twitter.com/TheBCHPodcast" target="_blank">
         <img src={twitterLogo} height={40} width={40} alt="Twitter link" />
+        </a>
+      </span>
+      <span style={spanStyle}>
+      <a href="https://t.me/thebitcoincashpodcast_discussion" target="_blank">
+        <img src={telegramLogo} height={40} width={40} alt="Twitter link" />
         </a>
       </span>
     </div>
