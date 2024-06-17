d20f799c6c41d3ec03321bc8fdf9498ea2de70ef
Jeremy <jeremy@Admins-MacBook-Pro.local>
2024-06-17 15:38:44 +0100

    Add Apple Podcasts.

--- src/theme/AnnouncementBar/Content/index.js
+++ src/theme/AnnouncementBar/Content/index.js
@@ -7,55 +7,61 @@ import redditLogo from "@site/static/img/socialMedia/reddit.png";
 import spotifyLogo from "@site/static/img/socialMedia/spotify.png";
 import twitchLogo from "@site/static/img/socialMedia/twitch.png";
 import tiktokLogo from "@site/static/img/socialMedia/tiktok.png";
-
+import applePodcastsLogo from "@site/static/img/socialMedia/applePodcasts.png";
 
 export default function AnnouncementBarContent() {
-  const styles = { height: 60, minHeight: 60, width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'  }
+  const styles = { height: 60, minHeight: 60, width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }
 
   const spanStyle = { paddingLeft: 5, paddingRight: 5 }
 
-return <div style={styles}>
+  return <div style={styles}>
     <div>
       <span style={spanStyle}>
         <a href="https://www.youtube.com/channel/UCsrDsJnHFnkMnJhEslofyPQ" target="_blank">
-        <img src={youtubeLogo} height={40} width={40} alt="Youtube link" />
+          <img src={youtubeLogo} height={40} width={40} alt="Youtube link" />
         </a>
       </span>
       <span style={spanStyle}>
-      <a href="https://twitter.com/TheBCHPodcast" target="_blank">
-        <img src={twitterLogo} height={40} width={40} alt="Twitter link" />
+        <a href="https://twitter.com/TheBCHPodcast" target="_blank">
+          <img src={twitterLogo} height={40} width={40} alt="Twitter link" />
         </a>
       </span>
       <span style={spanStyle}>
-      <a href="https://t.me/thebitcoincashpodcast_discussion" target="_blank">
-        <img src={telegramLogo} height={40} width={40} alt="Telegram link" />
+        <a href="https://t.me/thebitcoincashpodcast_discussion" target="_blank">
+          <img src={telegramLogo} height={40} width={40} alt="Telegram link" />
         </a>
       </span>
       <span style={spanStyle}>
-      <a href="https://www.twitch.tv/thebitcoincashpodcast" target="_blank">
-        <img src={twitchLogo} height={40} width={40} alt="Twitch link" />
+        <a href="https://www.twitch.tv/thebitcoincashpodcast" target="_blank">
+          <img src={twitchLogo} height={40} width={40} alt="Twitch link" />
         </a>
       </span>
       <span style={spanStyle}>
-      <a href="https://www.instagram.com/thebchpodcast/" target="_blank">
-        <img src={instagramLogo} height={40} width={40} alt="Instagram link" />
+        <a href="https://www.instagram.com/thebchpodcast/" target="_blank">
+          <img src={instagramLogo} height={40} width={40} alt="Instagram link" />
         </a>
       </span>
       <span style={spanStyle}>
-      <a href="https://www.tiktok.com/@thebchpodcast" target="_blank">
-        <img src={tiktokLogo} height={40} width={40} alt="TikTok link" />
+        <a href="https://www.tiktok.com/@thebchpodcast" target="_blank">
+          <img src={tiktokLogo} height={40} width={40} alt="TikTok link" />
         </a>
       </span>
       <span style={spanStyle}>
-      <a href="https://www.reddit.com/r/BitcoinCashPodcast/" target="_blank">
-        <img src={redditLogo} height={40} width={40} alt="Reddit link" />
+        <a href="https://www.reddit.com/r/BitcoinCashPodcast/" target="_blank">
+          <img src={redditLogo} height={40} width={40} alt="Reddit link" />
         </a>
       </span>
       <span style={spanStyle}>
-      <a href="https://open.spotify.com/show/01N17Oi7sRZM4vYGiTO6uk" target="_blank">
-        <img src={spotifyLogo} height={40} width={40} alt="Spotify link" />
+        <a href="https://open.spotify.com/show/01N17Oi7sRZM4vYGiTO6uk" target="_blank">
+          <img src={spotifyLogo} height={40} width={40} alt="Spotify link" />
         </a>
       </span>
+      <span style={spanStyle}>
+        <a href="https://podcasts.apple.com/gb/podcast/the-bitcoin-cash-podcast/id1555355070" target="_blank">
+          <img src={applePodcastsLogo} height={40} width={40} alt="Apple Podcasts link" />
+        </a>
+      </span>
+
     </div>
-  </div>
+  </div >
 }
